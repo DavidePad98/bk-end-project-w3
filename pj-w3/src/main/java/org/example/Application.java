079c02d5e004977b5dfa5393dfa2398c3258b22d
Davide Paderi 98 <151513202+DavidePad98@users.noreply.github.com>
2024-03-22 09:55:54 +0100

    start application with persistence

---
+++ pj-w3/src/main/java/org/example/Application.java
@@ -0,0 +1,13 @@
+package org.example;
+
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.Persistence;
+
+public class Application {
+    private static final EntityManagerFactory emf = Persistence.createEntityManagerFactory("u4d14");
+    public static void main(String[] args) {
+        EntityManager em = emf.createEntityManager();
+
+    }
+}
